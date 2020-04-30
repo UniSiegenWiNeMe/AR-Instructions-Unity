f539fb19045894e64a205e8beb934bdf544c61a0
Florian Jasche <FlorianJa@users.noreply.github.com>
2020-04-30 19:02:41 +0200

    Added fixed axis move controller

---
+++ Assets/UpdateSize.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateSize : MonoBehaviour
+{
+
+    public Collider HostCollider;
+    public bool UpdateXAxis;
+    public bool UpdateYAxis;
+    public bool UpdateZAxis;
+    public float ScalingFactor = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newScale = transform.localScale;
+
+        if (UpdateXAxis)
+        {
+            newScale.x =  ((HostCollider.bounds.extents.x ) / HostCollider.transform.localScale.x )* ScalingFactor;
+        }
+        if (UpdateYAxis)
+        {
+            newScale.y = ((HostCollider.bounds.extents.y ) / HostCollider.transform.localScale.y) * ScalingFactor;
+        }
+        if (UpdateZAxis)
+        {
+            newScale.z = ((HostCollider.bounds.extents.z ) / HostCollider.transform.localScale.z) * ScalingFactor;
+        }
+
+        transform.localScale = newScale;
+    }
+}
